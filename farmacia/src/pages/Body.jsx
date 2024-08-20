286146e3719c3feee9f2a99ad8b37095ed6e8f03
AlexandrosRodrigue <alexandros_kotzias@estudante.sesisenai.org.br>
2024-08-20 17:21:24 -0300

    aula

--- farmacia/src/pages/Body.jsx
+++ farmacia/src/pages/Body.jsx
@@ -1,19 +1,48 @@
+import React, { useEffect, useState } from 'react';
 import './Body.css';
 
 function Body() {
+  const [currentIndex, setCurrentIndex] = useState(0);
+  const totalSlides = 3; // Número total de slides
+
+  useEffect(() => {
+    const interval = setInterval(() => {
+      setCurrentIndex((currentIndex + 1) % totalSlides);
+    }, 3000);
+
+    return () => clearInterval(interval);
+  }, [currentIndex]);
+
+  const showSlide = (index) => {
+    setCurrentIndex(index);
+  };
+
   return (
     <div className="Body-container">
-     <h3>Estamos animados em anunciar que nossa farmácia está sob nova direção! Com o novo proprietário, estamos mais comprometidos do que nunca em oferecer a você o melhor atendimento, produtos de alta qualidade e um serviço de saúde personalizado que você merece.
-
-Por que visitar nossa farmácia?
-
-Atendimento atencioso e qualificado
-Variedade de medicamentos e produtos de bem-estar
-Consultas farmacêuticas personalizadas
-Ofertas e descontos exclusivos de inauguração
-Estamos aqui para cuidar de você e da sua família. Venha nos visitar e confira as novidades! Estamos ansiosos para recebê-lo em nossa nova fase.</h3>
+      <div className="bodyCentral">
+        <div className="carousel-container">
+          <div
+            className="carousel-slide"
+            style={{ transform: `translateX(${-currentIndex * 100}%)` }}
+          >
+            <img src="/imagens/farmacia.PNG.png" alt="Slide 1" />
+<img src="/imagens/farmacia.PNG.png" alt="Slide 2" />
+<img src="/imagens/farmacia.PNG.png" alt="Slide 3" />
+          </div>
+          <div className="carousel-indicators">
+            {[...Array(totalSlides)].map((_, index) => (
+              <div
+                key={index}
+                className={`indicator ${index === currentIndex ? 'active' : ''}`}
+                onClick={() => showSlide(index)}
+                data-index={index}
+              ></div>
+            ))}
+          </div>
+        </div>
+      </div>
     </div>
-  )
+  );
 }
 
-export default Body
+export default Body;
--- farmacia/src/pages/Header.jsx
+++ farmacia/src/pages/Header.jsx
@@ -2,10 +2,17 @@ import './Header.css';
 
 function Header() {
   return (
+    <div className="container">
     <div className="header-container">
       <div className="logo">
         <img src="../../imagens/farmacia.PNG.png" alt="" className='imgLogo'/>
         <h3 className='titulo'>Farmacia do Alex</h3>
+      </div>
+      <div className="pesquisa">
+        <input type="text" id='inputPesquisa' placeholder='O que você está procurando?'/>
+        <div className="caixa">
+        <i class="fa-solid fa-magnifying-glass"></i>
+        </div>
       </div>
       <div className='resto'>
         <div className="icons">
@@ -14,10 +21,25 @@ function Header() {
         <div className="icons">
         <i class="fa-solid fa-money-bill-wave"></i>
         </div>
-        <div className="icons">
-        <i class="fa-solid fa-bullseye"></i>
-        </div>
       </div>
+    </div>
+    <div className="navBar">
+      <div className="caminhos">
+        <button className='btnTitulos'><span className="nomeBtn">Ofertas</span><i class="fa-solid fa-caret-down"></i></button>
+      </div>
+      <div className="caminhos">
+        <button className='btnTitulos'><span className="nomeBtn">Infantil</span><i class="fa-solid fa-caret-down"></i></button>
+      </div>
+      <div className="caminhos">
+        <button className='btnTitulos'><span className="nomeBtn">Adulto</span><i class="fa-solid fa-caret-down"></i></button>
+      </div>
+      <div className="caminhos">
+        <button className='btnTitulos'><span className="nomeBtn">Idoso</span><i class="fa-solid fa-caret-down"></i></button>
+      </div>
+      <div className="caminhos">
+        <button className='btnTitulos'><span className="nomeBtn">Maquiagens</span><i class="fa-solid fa-caret-down"></i></button>
+      </div>
+    </div>
     </div>
   )
 }
